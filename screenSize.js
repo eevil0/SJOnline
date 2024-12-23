a8f1680159c92c149e9f0d43eb470883d9dc4d47
Dezső KOZMA <dezso.kozma@xapt.com>
2024-12-23 19:40:24 +0100

    cross-browser with throttling

--- screenSize.js
+++ screenSize.js
@@ -1,12 +1,35 @@
-﻿window.screenChangeInterop = {
+﻿const getScreenDimensions = () => {
+    return {
+        width:
+            window.innerWidth ||
+            document.documentElement.clientWidth ||
+            document.body.clientWidth,
+        height:
+            window.innerHeight ||
+            document.documentElement.clientHeight ||
+            document.body.clientHeight,
+    };
+};
+
+window.screenChangeInterop = {
     addChangeListener: function (dotNetReference) {
+
         const notifyChange = () => {
-            dotNetReference.invokeMethodAsync("UpdateScreenState", window.innerWidth, window.innerHeight);
+            const dimensions = getScreenDimensions();
+            dotNetReference.invokeMethodAsync("UpdateScreenState", dimensions.width, dimensions.height);
+        };
+
+        let resizeTimeout;
+
+        // Use throttling to prevent rapid firing of events
+        const throttledNotifyChange = () => {
+            clearTimeout(resizeTimeout);
+            resizeTimeout = setTimeout(notifyChange, 200); // 200ms delay
         };
 
         // Attach event listeners
-        window.addEventListener("resize", notifyChange);
-        window.addEventListener("orientationchange", notifyChange);
+        window.addEventListener("resize", throttledNotifyChange);
+        window.addEventListener("orientationchange", throttledNotifyChange);
 
         // Trigger the check on initial load
         notifyChange();
