7db81c1ee80c31e7b8bb47291f9bec232dd0aa0d
Dezső KOZMA <dezso.kozma@xapt.com>
2024-12-23 17:37:24 +0100

    screen width and height realtime event listener

---
+++ screenSize.js
@@ -0,0 +1,20 @@
+﻿window.screenChangeInterop = {
+    addChangeListener: function (dotNetReference) {
+        const notifyChange = () => {
+            dotNetReference.invokeMethodAsync("UpdateScreenState", window.innerWidth, window.innerHeight);
+        };
+
+        // Attach event listeners
+        window.addEventListener("resize", notifyChange);
+        window.addEventListener("orientationchange", notifyChange);
+
+        // Trigger the check on initial load
+        notifyChange();
+    },
+
+    removeChangeListener: function () {
+        // Remove event listeners
+        window.removeEventListener("resize", this.notifyChange);
+        window.removeEventListener("orientationchange", this.notifyChange);
+    }
+};
